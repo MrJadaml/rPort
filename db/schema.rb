71dd153def3f1875faccc90c9a6301474cb33c3e
mrjadaml <mrjadaml@gmail.com>
2015-04-11 15:27:54 -0600

    added carrierwave gem and created uploader for project model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411130802) do
+ActiveRecord::Schema.define(version: 20150411212453) do
 
   create_table "messages", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150411130802) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
+    t.string "avatar"
   end
 
 end

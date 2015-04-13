fe108a1816741d5dfcc887f1217b460d3d372a8b
mrjadaml <mrjadaml@gmail.com>
2015-04-13 13:02:37 -0600

    added :link attr to project

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,6 +17,15 @@ class ProjectsController < ApplicationController
     @project = Project.find(params[:id])
   end
 
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to :root
+    else
+      render :edit
+    end
+  end
+
   def destroy
     project = Project.find(params[:id])
     if project.destroy
@@ -29,6 +38,6 @@ class ProjectsController < ApplicationController
 private
 
   def project_params
-    params.require(:project).permit(:title, :avatar, :avatar_cache)
+    params.require(:project).permit(:title, :link, :avatar, :avatar_cache)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411212453) do
+ActiveRecord::Schema.define(version: 20150413175529) do
 
   create_table "messages", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150411212453) do
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.string "avatar"
+    t.string "link"
   end
 
 end

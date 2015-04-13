5ea8392c54718d8ddfe43d5a4c2d8428da0cd313
mrjadaml <mrjadaml@gmail.com>
2015-04-13 09:08:56 -0600

    create action for project controller, deleted application css from pub/assets due to caching bug, images from newed up projects working

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,4 +4,14 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
+  def create
+    project = Project.create(project_params)
+    redirect_to :root if project.save
+  end
+
+private
+
+  def project_params
+    params.require(:project).permit(:title, :avatar, :avatar_cache)
+  end
 end
